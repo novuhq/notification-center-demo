2af0d25080d2733735c31db6e0c1c0572bb36d5f
Raman Yasel <strogofcdm@gmail.com>
2022-10-14 17:41:38 +0300

    feat: update header icons and NovuProvider theme

--- src/components/shared/header/header.jsx
+++ src/components/shared/header/header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Button from 'components/shared/button';
 import Link from 'components/shared/link';
 import Novu from 'helpers/novu';
-import DarkThemeIcon from 'icons/dark-theme.inline.svg';
+import LightThemeIcon from 'icons/light-theme.inline.svg';
 import Logo from 'icons/logo.inline.svg';
 import MagnifierIcon from 'icons/magnifier.inline.svg';
 import NavigationIllustration from 'icons/nav-illustration.inline.svg';
@@ -22,8 +22,8 @@ const Header = () => (
           <MagnifierIcon className="w-5 sm:w-3" aria-hidden />
         </Button>
         <Button theme="rounded" type="button">
-          <span className="sr-only">Dark theme icon</span>
-          <DarkThemeIcon className="w-5 sm:w-3" aria-hidden />
+          <span className="sr-only">Light theme icon</span>
+          <LightThemeIcon className="w-5 sm:w-3" aria-hidden />
         </Button>
         <div className="flex h-[46px] w-[46px] items-center justify-center rounded-full border border-grey-4 bg-white sm:h-[30px] sm:w-[30px]">
           <Novu />
--- src/helpers/novu.jsx
+++ src/helpers/novu.jsx
@@ -1,9 +1,26 @@
-import {
-  NovuProvider,
-  PopoverNotificationCenter,
-  NotificationBell,
-} from '@novu/notification-center';
+import { NovuProvider, PopoverNotificationCenter } from '@novu/notification-center';
 import React, { useCallback } from 'react';
+
+import BellIcon from 'icons/bell.inline.svg';
+
+const theme = {
+  dark: {
+    layout: {
+      background: '#1A1A1A',
+    },
+    header: {
+      badgeColor: '#FFE14D',
+      badgeTextColor: '#000',
+    },
+    notificationItem: {
+      unseen: {
+        background: '#262626',
+        notificationItemBeforeBrandColor: '#FFE14D',
+      },
+    },
+    loaderColor: '#FFE14D',
+  },
+};
 
 const Novu = () => {
   const onClick = useCallback((notification) => {
@@ -15,9 +32,17 @@ const Novu = () => {
       subscriberId={process.env.NEXT_PUBLIC_NOVU_SUBSCRIBER_ID}
       applicationIdentifier={process.env.NEXT_PUBLIC_NOVU_APP_ID}
     >
-      <PopoverNotificationCenter colorScheme="dark" onNotificationClick={onClick}>
+      <PopoverNotificationCenter colorScheme="dark" theme={theme} onNotificationClick={onClick}>
         {({ unseenCount }) => (
-          <NotificationBell unseenCount={unseenCount} aria-label="Notifications" />
+          <>
+            <BellIcon className="w-6 cursor-pointer" aria-label="Notifications bell icon" />
+            {unseenCount ? (
+              <span
+                className="absolute top-0 right-0 z-10 block h-3 w-3 rounded-full border border-white bg-purple"
+                aria-label="Icon number of unread notifications"
+              />
+            ) : null}
+          </>
         )}
       </PopoverNotificationCenter>
     </NovuProvider>
